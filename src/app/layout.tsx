21300bfcab571a36f8702b5d592e5f1f076a0e24
VenkatLohithDasari <dennytrade3@gmail.com>
2025-02-27 03:03:42 +0530

    Apply consistent style to all over the site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-background-primary text-text-primary ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>{children}</AuthProvider>
       </body>
